26462cb14e3df937162f71f1be2314661bdeb503
Joel Eckert <joel@joelmeckert.com>
2022-08-07 17:20:52 -0400

    Simplified logic and regex
    Cleaner output upon errors, valid MTU determined by regex

--- jc-path-mtu.sh
+++ jc-path-mtu.sh
@@ -1,80 +1,84 @@
 #!/bin/bash
-# Joel Eckert, joel@joelme.ca, 2022-07-20
+# Joel Eckert, joel@joelme.ca, 2022-08-07
 
-# Usage:
+# Usage, JSON output:
 # ./jc-path-mtu.sh contoso.com
 # ./jc-path-mtu.sh contoso.com 1492
 # ./jc-path-mtu.sh contoso.com 1500 0.2
 # ./jc-path-mtu.sh contoso.com 1450 .05
+
+# Usage, example with jq returning integer
+# ./jc-path-mtu.sh contoso.com | jq .mtu
 
 # Adjust the maximum and minimum MTU, and ICMP timeout as required for higher latency links, default is 0.1, 100 ms
 mtu_maximum=1500
 mtu_minimum=500
 overhead=28
 bytes_minimum=$(( $mtu_minimum + $overhead ))
-timeout_minimum=0.02
 timeout_default=0.1
-rx_timeout='^([0-9]?)?(\.([0-9])?|\.[0-9][0-9]?)$'
+rx_timeout='^([0-9]?)?(\.([0-9])?|\.[0-9][2-9]?)$'
 
 # If 3rd timeout argument is specified, validate it with regex, set the default upon failure
-if [[ -z "${3}" ]]; then
-	timeout=$timeout_default
+if [[ "${3}" =~ $rx_timeout ]]; then
+	timeout=$3
 else
-	if [[ $3 =~ $rx_timeout ]]; then
-		if [[ $3 > $timeout_minimum ]]; then
-			timeout=$3
-		else
-			timeout=$timeout_minimum
-		fi
-	else
-		timeout=$timeout_default
-	fi
+	timeout=$timeout_default
 fi
 
-# Validate that the specifid MTU is a maximum of four numeric integers
-rx_mtu='^[0-9]{3,4}$'
+# Validate that the specifid MTU is a maximum of 1500
+rx_mtu='^(([1][0-4][0-9]{2})|([1-9][0-9]{2}))$'
 
 # Remote host
 host="${1}"
 
+# Link to jc and jq on GitHub
+jcuri='https://github.com/kellyjonbrazil/jc'
+jquri='https://github.com/stedolan/jq'
+jc_path_mtu="https://github.com/joelmeckert/jc-path-mtu"
+
 # Determines the location of jc, if it exists, sets the path to the jqpath variable
 jcpath=$(which jc)
 if [[ $? -ne 0 ]]; then
-	echo -e "\e[31m\e[1mERROR\e[0m\t\e[4mjc not found\e[0m at \e[1m${jcsearchpath1}\e[0m \e[33mor\e[0m \e[1m${jcsearchpath2}\e[0m\n\e[1mLink\t\e[34m\e[4m${jcuri}\e[0m" >&2
-	exit 1
+	echo -e "\e[31m\e[1mERROR\t\e[0mjc not found\e[1m\t\e[34m\e[4m${jcuri}\e[0m" >&2
+	exitflag=1
 fi
 
 # Determines the location of jq, if it exists, sets the path to the jqpath variable
 jqpath=$(which jq)
 if [[ $? -ne 0 ]]; then
-	echo -e "\e[31m\e[1mERROR\e[0m\t\e[4mjq not found\e[0m at \e[1m${jqsearchpath1}\e[0m \e[33mor\e[0m \e[1m${jqsearchpath2}\e[0m\n\e[1mLink\t\e[34m\e[4m${jquri}\e[0m" >&2
+	echo -e "\e[31m\e[1mERROR\t\e[0mjq not found\e[1m\t\e[34m\e[4m${jquri}\e[0m" >&2
+	exitflag=1
+fi
+
+if [[ $exitflag -eq 1 ]]; then
+	echo -e "\t\e[1mjc-path-mtu\t\e[1m\e[34m\e[4m${jc_path_mtu}\e[0m" >&2
 	exit 1
 fi
 
 # Determine if the host is reachable via ICMP
 attempts=2
-bytes=56
-result=$(ping -c $attempts -M do -s $bytes -W $timeout "${host}" | jc --ping -p | jq)
+bytes_test=56
+result=$(ping -c $attempts -M do -s $bytes_test -W $timeout "${host}" | jc --ping -p | jq)
 egress=$(echo "${result}" | jq .packets_transmitted)
 ingress=$(echo "${result}" | jq .packets_received)
 lost=$(($egress - $ingress))
 
+# If MTU is specified as the second argument, calculate overhead, if not specified, calculate overhead based on max MTU
+if [[ "${2}" =~ $rx_mtu ]]; then
+	bytes=$(($2 - $overhead))
+else
+	bytes=$(($mtu_maximum - $overhead))
+fi
+
 # If the host responds to ICMP, proceed with the tests
 if [[ $lost -eq 0 ]]; then
-
-	# If MTU is specified as the second argument, calculate overhead, if not specified, calculate overhead based on max MTU
-	if [[ -n $2 ]] && [[ $2 =~ $rx_mtu ]] && [[ $2 < $mtu_maximum ]]; then
-		bytes=$(($2 - $overhead))
-	else
-		bytes=$(($mtu_maximum - $overhead))
-	fi
-
+	
 	# Set packets lost to 1
 	lost=1
-
+	
 	# Loop until there is an ICMP response, decrementing by one byte each loop where required
 	while [[ $lost -gt 0 ]] && [[ $bytes -ge $bytes_minimum ]]; do
-
+	
 		attempts=1
 		result=$(ping -c $attempts -M do -s $bytes -W $timeout "${host}" | jc --ping -p | jq)
 		egress=$(echo "${result}" | jq .packets_transmitted)
@@ -84,7 +88,7 @@ if [[ $lost -eq 0 ]]; then
 		# If there is packet loss, decrement by one byte
 		if [[ $lost -gt 0 ]]; then
 			bytes=$(($bytes - 1))
-
+		
 		# If single echo is successful, test for two echo replies, packet loss should be zero
 		else
 			attempts=2
@@ -102,34 +106,23 @@ if [[ $lost -eq 0 ]]; then
 				epoch=$EPOCHSECONDS
 			fi
 		fi
-
+		
 	done
+fi
 
-	# If successful above, calculate the MTU, if it failed, return a zero integer value
-	if [[ $success -eq 1 ]]; then
-		# Calculate MTU from ICMP size
-		mtu=$(($bytes + $overhead))
-	else
-		# Output error to stderr, failed to discover MTU
-		echo -e "\e[31m\e[1mFAILED\e[0m\tMTU could not be reliably determined:\t${host}\e[0m" >&2
-	fi
-
-# Output error to stderr, failed to discover MTU, if jcpath and jqpath exist and processing could continue
+# If successful above, calculate the MTU, if it failed, return a zero integer value
+if [[ $success -eq 1 ]]; then
+	# Calculate MTU from ICMP size
+	mtu=$(($bytes + $overhead))
 else
-
-	if [[ -f "${jcpath}" ]] && [[ -f "${jqpath}" ]]; then
-		echo -e "\e[31m\e[1mERROR\e[0m\tMTU discovery failed for host, ICMP echo reply blocked or address is invalid:\t${host}\e[0m" >&2
-	fi
-
-fi
-
-# If the epoch variable is not set, as the test was not successful, set it now
-if [[ -z $epoch ]]; then
 	epoch=$EPOCHSECONDS
-	# Return a zero integer value for the MTU, so that the data returns an integer and not a null value for the monitoring system
 	mtu=0
+	exitflag=1
+	# Output error to stderr, failed to discover MTU
+	echo -e "\e[31m\e[1mFAILED\e[0m\tMTU could not be reliably determined:\t${host}\e[0m" >&2
 fi
 
+# Set datetime variable from epoch seconds, ISO 8601
 datetime=$(date -d @$epoch +"%Y-%m-%dT%T%:z")
 
 # Create JSON
@@ -137,3 +130,7 @@ json=$(echo -e '{'"\n\t"'"date": "'$datetime'"'",\n\t"'"host": "'$host'"'",\n\t"
 
 # Output to stdout via jq
 echo "${json}" | jq
+
+if [[ $exitflag -eq 1 ]]; then
+	exit 1
+fi
